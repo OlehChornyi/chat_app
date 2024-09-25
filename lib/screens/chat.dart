ab43942ca58e7998722cde8734b4f028f2c01834
oleh <phd.chornyi@gmail.com>
2024-09-25 12:58:25 +0300

    4th: splash and logout

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,15 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
-  
-@override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(Icons.exit_to_app, color: Theme.of(context).colorScheme.primary,),
+          ),
+        ],
       ),
-      body: const Center(child: Text('Loged In'),),
+      body: const Center(
+        child: Text('Loged In'),
+      ),
     );
   }
 }
